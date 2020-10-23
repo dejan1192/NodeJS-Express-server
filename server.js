cee6e598c1a674d323c7ee9c5cf38c01df80c5ce
dejan1192 <dejan1192@gmail.com>
2020-10-23 13:32:17 +0200

    Added multer & changed allow-access-controll-headers

--- server.js
+++ server.js
@@ -5,20 +5,40 @@ const authRoutes = require('./routes/authRoutes');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const postRoutes = require('./routes/postRoutes');
+const multer = require('multer');
+const path = require('path');
+const profileRoutes = require('./routes/profileRoutes');
+
 dotenv.config();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    res.header('Access-Control-Allow-Headers', 'Origin , X-Requested-With, Content-Type, Authorization, Accept');
-    next();
+const storage = multer.diskStorage({
+    destination:function(req, file , cb){
+        cb(null, __dirname + '/public/uploads')
+    },
+    filename:function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
 })
+
+app.use(multer({storage:storage}).single('image'));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With, Content-Type,Authorization, Accept')
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({})
+    }
+    next();
+});
 app.use(cookieParser());
 app.use(express.json());
 
+app.use(express.static('public'));
 app.use(postRoutes);
 app.use('/auth', authRoutes);
-
+app.use('/profile', profileRoutes);
 
 app.use((error, req, res, next) => {
 
