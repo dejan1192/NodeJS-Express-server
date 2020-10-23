d7079400f8476a0a351f4619343353f6b27bab0b
dejan1192 <dejan1192@gmail.com>
2020-10-23 13:45:37 +0200

    Added imageUrl property in User model

--- models/User.js
+++ models/User.js
@@ -17,6 +17,10 @@ const userSchema = new Schema({
         type:String,
         required:true,
     },
+    imageUrl:{
+        type:String,
+        required:false
+    },
     posts:[
         {
             type:Schema.Types.ObjectId,
